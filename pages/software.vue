c2fa45e8c9c9a583b1d3eeea6448269b79e9ce50
Ben Chomsang <43471788+1994benc@users.noreply.github.com>
2022-06-23 01:54:43 +0100

    add software page

---
+++ pages/software.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1 class="text-7xl">I build things with code</h1>
+    <div class="my-8 space-y-6">
+      <div>
+        I am a full-stack web engineer with love for data science. My native
+        language is TypeScript.
+      </div>
+      <ComingSoonMessage message="This is where I'll showcase my work" />
+    </div>
+  </div>
+</template>
