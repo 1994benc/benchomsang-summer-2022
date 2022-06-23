60ef18fb29f006a839b9383227133f88a7634e7f
Ben Chomsang <43471788+1994benc@users.noreply.github.com>
2022-06-23 01:56:39 +0100

    Navigation from the Intro page - add links - add keydown handler

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="p-16 rounded-xl mx-auto border max-w-5xl">
+  <div class="container">
     <h1 class="text-7xl">Hello friend 😊 What do you want to know about me?</h1>
     <div class="my-8">
-      <button class="button">Software engineering <KeyboardShortcutBadge label="Press A" /></button>
-      <button class="button">My house plants 🪴 <KeyboardShortcutBadge label="Press B" /></button>
-      <button class="button">My journey <KeyboardShortcutBadge label="Press C" /></button>
+      <NuxtLink to="/software"
+        ><button class="button">
+          Software engineering <KeyboardShortcutBadge label="Press A" /></button
+      ></NuxtLink>
+      <NuxtLink to="/plants"
+        ><button class="button">
+          My house plants 🪴 <KeyboardShortcutBadge label="Press B" /></button
+      ></NuxtLink>
+      <NuxtLink to="/journey"
+        ><button class="button">
+          My journey <KeyboardShortcutBadge label="Press C" /></button
+      ></NuxtLink>
     </div>
   </div>
 </template>
 
 <script>
-import KeyboardShortcutBadge from './KeyboardShortcutBadge.vue';
+import KeyboardShortcutBadge from "./KeyboardShortcutBadge.vue";
+
+const handleUserKeyDown = function(e) {
+  if (e.key === "a") {
+    this.$router.push("/software");
+  } else if (e.key === "b") {
+    this.$router.push("/plants");
+  } else if (e.key === "c") {
+    this.$router.push("/journey");
+  }
+};
 export default {
-    name: "Intro",
-    components: { KeyboardShortcutBadge }
+  name: "Intro",
+  components: { KeyboardShortcutBadge },
+  mounted() {
+    console.log("Intro created");
+    window.addEventListener("keydown", handleUserKeyDown.bind(this));
+  },
+  beforeDestroy() {
+    console.log("Intro destroyed");
+    window.removeEventListener("keydown", handleUserKeyDown.bind(this));
+  },
 };
 </script>
