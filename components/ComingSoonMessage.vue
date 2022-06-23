668e9064b80c494028c68de36ed801bed07aa20c
Ben Chomsang <43471788+1994benc@users.noreply.github.com>
2022-06-23 01:54:03 +0100

    Add ComingSoonMessage component

---
+++ components/ComingSoonMessage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-yellow-50 p-10 text-yellow-500 rounded">
+    <span class="font-black mr-4">Coming soon</span> {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComingSoonMessage",
+  props: {
+    message: String,
+  },
+};
+</script>
